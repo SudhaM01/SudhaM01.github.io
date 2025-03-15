afcd3fa77e3890d015ffa59b8e1166646d4a3df9
SudhaM01 <muppalasudha@gmail.com>
2025-03-15 12:26:29 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById("themeToggle").addEventListener("click", function() {
-    document.body.classList.toggle("dark-mode");
-});
+// Open Project Modal
+function openModal(projectFile) {
+    document.getElementById("modalFrame").src = projectFile;
+    document.getElementById("projectModal").style.display = "block";
+}
+
+// Close Modal
+function closeModal() {
+    document.getElementById("projectModal").style.display = "none";
+    document.getElementById("modalFrame").src = "";
+}
